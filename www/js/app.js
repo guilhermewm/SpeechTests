045189d5677e136419e78941017c277ea4f33067
João Vitor Machado <jvmachado@cipucrs.pucrs.br>
2016-11-04 11:26:35 -0200

    adicionada funcionalidade para responder user, mudar cor e falar texto

--- www/js/app.js
+++ www/js/app.js
@@ -11,7 +11,15 @@ angular.module('starter', ['ionic'])
     speechText: ''
   };
   $scope.recognizedText = '';
-
+  $scope.dic = {
+    cor:{
+      preto:"#000",
+      branco:"#fff",
+      vermelho:"#ff0000",
+      verde:"#00ff00",
+      azul:"#00f"
+    }
+  };
   $scope.speakText = function() {
     TTS.speak({
            text: $scope.data.speechText,
@@ -23,14 +31,30 @@ angular.module('starter', ['ionic'])
            // Handle the error case
        });
   };
-
   $scope.record = function() {
     var recognition = new SpeechRecognition();
     recognition.lang = 'pt-BR';
     recognition.onresult = function(event) {
         if (event.results.length > 0) {
             $scope.recognizedText = event.results[0][0].transcript;
-            $scope.$apply()
+            $scope.split = $scope.recognizedText.split(" ");
+            $scope.split.map(function(p,index, array){
+                if(p === "cor"){
+                  var corEscolhida = array[index+1];
+                  alert(corEscolhida);
+                  $scope.color = $scope.dic.cor[corEscolhida];
+                }
+                if(p==="falar"){
+                  var fala =   $scope.recognizedText.split("falar ")[1];
+                  $scope.data.speechText = fala;
+                  $scope.speakText();
+                }
+                if(p==="Jarvis"){
+                  $scope.data.speechText = "Como posso ajudá-lo João O Todo Poderoso?";
+                  $scope.speakText();
+                }
+            })
+            $scope.$apply();
         }
     };
     recognition.start();
