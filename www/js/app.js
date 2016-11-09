3f71f00e917c85df1f5ffef341aba620e3d64032
João Vitor Machado <jvmachado@cipucrs.pucrs.br>
2016-11-09 15:32:32 -0200

    version 1.1

--- www/js/app.js
+++ www/js/app.js
@@ -21,10 +21,12 @@ angular.module('starter', ['ionic'])
       azul:"#00f"
     }
   };
+  $scope.lang = navigator.language || navigator.userLanguage;
   $scope.speakText = function(text) {
+    var lang = navigator.language || navigator.userLanguage;
     TTS.speak({
            text: text,
-           locale: 'pt-BR',
+           locale: lang,
            rate: 1.5
        }, function () {
            // Do Something after success
@@ -32,28 +34,56 @@ angular.module('starter', ['ionic'])
            // Handle the error case
        });
   };
+  $scope.speakTextColor = function(text, callback) {
+    TTS.speak({
+           text: text,
+           locale: $scope.lang,
+           rate: 1.5
+       }, function () {
+           callback();
+       }, function (reason) {
+           // Handle the error case
+       });
+  };
   $scope.items = [];
   $scope.record = function() {
     var recognition = new SpeechRecognition();
-    recognition.lang = 'pt-BR';
+    recognition.lang = $scope.lang;
     recognition.onresult = function(event) {
         if (event.results.length > 0) {
             $scope.recognizedText = event.results[0][0].transcript;
             $scope.recognizedText = $scope.recognizedText.toLowerCase();
             $scope.split = $scope.recognizedText.split(" ");
+            ($scope.recognizedText );
             $scope.split.map(function(p,index, array){
               if(p === "jarvis"){
                 $scope.data.speechText = "Como posso Ajudar?";
                   $scope.speakText($scope.data.speechText);
                 $scope.$apply();
-                
+
               }else{
               switch(p){
                 case "cor":
                   var corEscolhida = array[index+1];
-                  corEscolhida = $scope.dic.cor[corEscolhida];
-                  if(corEscolhida === undefined){
+                  var hex = $scope.dic.cor[corEscolhida];
+                  if(hex === undefined){
+                    $scope.speakTextColor("Não reconheço a cor "+corEscolhida
+                    +", qual seria seu valor em hexadecimal? ",function(){
+                      var newColorRecognition = new SpeechRecognition();
+                      newColorRecognition.lang= $scope.lang;
+                      newColorRecognition.onresult = function(event){
+                        var newColor = event.results[0][0].transcript;
+                        newColor = newColor.replace(/\s+/g, '');
+                        $scope.dic.cor[corEscolhida] = "#"+newColor;
+                        $scope.color = $scope.dic.cor[corEscolhida];
+                        $scope.$apply();
 
+                        alert($scope.color);
+                      }
+                      newColorRecognition.start();
+                    });
+                  }else{
+                    $scope.color = $scope.dic.cor[corEscolhida];
                   }
                   $scope.$apply();
                 break;
@@ -68,21 +98,20 @@ angular.module('starter', ['ionic'])
                   $scope.speakText($scope.data.speechText);
                   $scope.$apply();
                 break;
-                case "adicione" && array[index+1] ==="o" && array[index+2] === "item":
-                  var name = $scope.recognizedText.split("adicione o item ")[1];
-                  var item = {
-                    name: name
+                case "adicione":
+                  if(array[index+1] ==="o" && array[index+2] === "item"){
+                    var name = $scope.recognizedText.split("adicione o item ")[1];
+                    var item = {
+                      name: name
+                    }
+                    $scope.items.push(item);
+                    $scope.$apply();
                   }
-                  $scope.items.push(item);
-                  $scope.$apply();
                 break;
 
               }
             }
-            init();
             })
-
-            // $scope.$apply();
         }
     };
     recognition.start();
@@ -90,6 +119,5 @@ angular.module('starter', ['ionic'])
  var init = function() {
     $scope.record();
   };
-  // init();
   $timeout(function(){init()}, 1500);
 });
